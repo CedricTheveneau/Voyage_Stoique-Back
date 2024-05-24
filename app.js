8b085690152425c48cd3064c9b66ad48fe56c728
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-05-24 14:21:41 +0200

    Updated back for api calls from front and deployment

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 // Importantion de la connexion à MongoDB
 require("./app/models/index.js");
 
 const router = require("./app/routes/index.js");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 //Ajout des routes
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 const app = require("./app.js");
 const port = process.env.PORT;
 const projectName = process.env.PROJECT_NAME;
+const apiSuccessRes = {
+  API: "Connected",
+  RESPONSE: `Success ! You're now live on ${projectName}`,
+};
 
 app.get("/api", (req, res) => {
-  res.send(`You're now live on ${projectName}`);
+  res.send(apiSuccessRes);
 });
 
 app.listen(port, () => {
